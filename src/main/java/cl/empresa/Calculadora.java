bff80a8375a64b8a73b4c9ab9aea7989e2dc2b9d
Marcelo Cáceres <marcelo.caceres@alumno.iplacex.cl>
2026-08-31 18:20:12 -0400

    chore: configuracion inicial del proyecto Maven

---
+++ src/main/java/cl/empresa/Calculadora.java
@@ -0,0 +1,15 @@
+package cl.empresa;
+
+public class Calculadora {
+
+
+public int sumar(int numero1, int numero2) {
+    return numero1 + numero2;
+}
+
+public int restar(int numero1, int numero2) {
+    return numero1 - numero2;
+}
+
+
+}
---
+++ src/test/java/cl/empresa/CalculadoraTest.java
@@ -0,0 +1,31 @@
+package cl.empresa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraTest {
+
+
+@Test
+void deberiaSumarCorrectamenteDosNumeros() {
+
+    Calculadora calculadora = new Calculadora();
+
+    int resultado = calculadora.sumar(5, 3);
+
+    assertEquals(8, resultado);
+}
+
+@Test
+void deberiaRestarCorrectamenteDosNumeros() {
+
+    Calculadora calculadora = new Calculadora();
+
+    int resultado = calculadora.restar(10, 4);
+
+    assertEquals(6, resultado);
+}
+
+
+}
